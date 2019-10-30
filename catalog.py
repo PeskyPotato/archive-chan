51df3bb442b41802009ab13c969d519bc7faaa3a
Lame Lemon <garybsantos@gmail.com>
2019-10-30 13:25:41 +0000

    added multithreading to catalogue

--- catalog.py
+++ catalog.py
@@ -1,12 +1,9 @@
-from bs4 import BeautifulSoup as soup 
 from archiver import archive
-from urllib.request import Request
-from urllib.request import urlopen
+from writer import boards
 import requests
-import json
 import argparse
 from time import time
-from models import Thread
+from multiprocessing import Process
 
 
 def catalog_threads(cat, verbose, preserve):
@@ -15,11 +12,17 @@ def catalog_threads(cat, verbose, preserve):
     if r.status_code == 200:
         data = r.json()
         for page in data:
+            processes = []
             for thread in page["threads"]:
                 t_id = thread["no"]
-                archive("http://boards.4chan.org/{}/thread/{}".format(cat, t_id), verbose, preserve)
+                process = Process(target=archive,
+                                args=("http://boards.4chan.org/{}/thread/{}".format(cat, t_id), verbose, preserve))
+                process.start()
+                processes.append(process)
+            for p in processes:
+                p.join()
     else:
-        print("Invalid request:",url)
+        print("Invalid request:", url)
 
 
 def main():
@@ -30,12 +33,15 @@ def main():
 
     parser = argparse.ArgumentParser(description="Archives 4chan threads")
     parser.add_argument("Board", help="Enter the board to download")
-    parser.add_argument("-p","--preserve_files", help="Save images and video files locally", action="store_true")
+    parser.add_argument("-p", "--preserve_files", help="Save images and video files locally", action="store_true")
     parser.add_argument("-v", "--verbose", help="Print more information on each post", action="store_true")
 
     args = parser.parse_args()
 
     cat = args.Board
+    if cat not in boards:
+        print("Board \"{}\" does not exist".format(cat))
+        return
     if args.preserve_files:
         p = True
     if args.verbose:
@@ -44,6 +50,6 @@ def main():
     catalog_threads(cat, v, p)
     print("Time elapsed:", str(time()-start_time) + "s")
 
+
 if __name__ == "__main__":
     main()
-
